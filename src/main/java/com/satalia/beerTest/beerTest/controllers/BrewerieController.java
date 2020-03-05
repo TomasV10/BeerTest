a89b23c39559524a8caf33061cfa8f5deb6dc34a
TomasV10 <tomasvarnauskas19@gmail.com>
2020-03-05 19:00:35 +0200

    edited controller, but not working properly yet.

--- src/main/java/com/satalia/beerTest/beerTest/controllers/BrewerieController.java
+++ src/main/java/com/satalia/beerTest/beerTest/controllers/BrewerieController.java
@@ -1,24 +1,25 @@
 package com.satalia.beerTest.beerTest.controllers;
 
 
+import com.satalia.beerTest.beerTest.dto.GeoLocationDto;
 import com.satalia.beerTest.beerTest.entities.Brewery;
 import com.satalia.beerTest.beerTest.entities.GeoLocation;
 import com.satalia.beerTest.beerTest.mainTask.DistanceMatrixCreator;
 import com.satalia.beerTest.beerTest.mainTask.NearestBrewery;
-import com.satalia.beerTest.beerTest.repositories.BreweriesRepository;
+import com.satalia.beerTest.beerTest.mainTask.Result;
 import com.satalia.beerTest.beerTest.repositories.LocationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 
-@RestController()
+@Controller
+@RequestMapping("/mvc/breweries")
 public class BrewerieController {
 
-    @Autowired
-    private BreweriesRepository breweriesRepository;
     @Autowired
     private NearestBrewery nearestBrewery;
     @Autowired
@@ -27,29 +28,30 @@ public class BrewerieController {
     private LocationsRepository locationsRepository;
 
 
-    @GetMapping("/loc")
-    public List<GeoLocation>getLocations(){
-      return locationsRepository.findAll();
+    @GetMapping("/home")
+    public String getFrom(Model model) {
+        GeoLocationDto geoLocationDto = new GeoLocationDto();
+        model.addAttribute("geoLocationDto", geoLocationDto);
+        return "homeLocationForm";
     }
 
-    @GetMapping("/all")
-    public double getAllBreweries() {
-        GeoLocation userInput = new GeoLocation(51.74250300, 19.43295600);
-        List<GeoLocation> locations = locationsRepository.findAll().stream().limit(10).collect(Collectors.toList());
-        locations.add(0, userInput);
-        double kazkas = nearestBrewery.nearestBrewery(locations, distanceMatrixCreator.calculateDistanceUsingMatrix(locations));
-        System.out.println(kazkas);
-        return kazkas;
-//        return new ArrayList<>(breweriesRepository.findAll());
-
+    @PostMapping(value = "/save")
+    public ModelAndView getCalculated(GeoLocationDto geoLocationDto, Model model){
+        GeoLocation location = geoLocationToDto(geoLocationDto);
+        List<GeoLocation>locations = locationsRepository.findAll();
+        locations.add(0, location);
+        Result result = nearestBrewery.nearestBrewery(locations, distanceMatrixCreator.calculateDistanceUsingMatrix(locations));
+        model.addAttribute("calculationResult", result);
+        return new ModelAndView("result");
     }
 
-    @GetMapping("/{id}")
-    public Optional<Brewery> getBreweryById(@PathVariable Long id) {
-        return breweriesRepository.findById(id);
+    private GeoLocation geoLocationToDto(GeoLocationDto geoLocationDto) {
+        GeoLocation location = new GeoLocation();
+        location.setId(geoLocationDto.getId());
+        location.setBrewery(geoLocationDto.getBrewery());
+        location.setLatitude(geoLocationDto.getLatitude());
+        location.setLongitude(geoLocationDto.getLongitude());
+        locationsRepository.save(location);
+        return location;
     }
-
-
-
-
 }
