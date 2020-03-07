43edfb8afd255dad48ffe0b5c3da093bbf029311
TomasV10 <tomasvarnauskas19@gmail.com>
2020-03-07 23:57:10 +0200

    made some changes with methods

--- src/main/java/com/satalia/beerTest/beerTest/mainTask/NearestBrewery.java
+++ src/main/java/com/satalia/beerTest/beerTest/mainTask/NearestBrewery.java
@@ -8,26 +8,42 @@ import com.satalia.beerTest.beerTest.entities.GeoLocation;
 import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
 public class NearestBrewery {
+
     public Result nearestBrewery(List<GeoLocation> breweryLoc, double[][] distanceMatrix) {
         final long startTime = System.nanoTime();
         double routeCost = 0;
-
         Route nearestRoute = new Route(breweryLoc.get(0));
 
+        routeCost = visitAsManyBreweriesAsPossibleInLimitedDistance(breweryLoc, distanceMatrix, routeCost, nearestRoute);
+
+        nearestRoute.getLocations().add(breweryLoc.get(0));
+        routeCost += distanceMatrix[breweryLoc.indexOf(nearestRoute.getHome())][breweryLoc.indexOf(nearestRoute.getCurrentBrewery())];
+
+        List<Brewery> breweries = getListOfBreweries(nearestRoute);
+
+        List<BeerType> collectedBeer = getListOfBeerTypes(breweries);
+
+        final long duration = (System.nanoTime() - startTime) / 1000000;
+        printResultToConsole(routeCost, breweries, collectedBeer, duration);
+        return new Result(routeCost, listOfbreweriesToDto(breweries), listOfBeerTypeToDto(collectedBeer), duration);
+    }
+
+    private double visitAsManyBreweriesAsPossibleInLimitedDistance(List<GeoLocation> breweryLoc, double[][] distanceMatrix, double routeCost, Route nearestRoute) {
         while (nearestRoute.getLocations().size() != breweryLoc.size()) {
             GeoLocation neighbourBrewery = null;
             double neighbourDistance = Double.MAX_VALUE;
 
             for (int i = 0; i < distanceMatrix.length; i++) {
                 for (int j = 0; j < distanceMatrix[i].length; j++) {
-
                     if (distanceMatrix[breweryLoc.indexOf(nearestRoute.getCurrentBrewery())][j] < neighbourDistance
                             && distanceMatrix[breweryLoc.indexOf(nearestRoute.getCurrentBrewery())][j] != 0
                             && !nearestRoute.getLocations().contains(breweryLoc.get(j))) {
+
                         neighbourBrewery = breweryLoc.get(j);
                         neighbourDistance = distanceMatrix[breweryLoc.indexOf(nearestRoute.getCurrentBrewery())][j];
                     }
@@ -43,65 +59,54 @@ public class NearestBrewery {
                 routeCost += neighbourDistance;
             }
         }
-        nearestRoute.getLocations().add(breweryLoc.get(0));
-        routeCost += distanceMatrix[breweryLoc.indexOf(nearestRoute.getHome())][breweryLoc.indexOf(nearestRoute.getCurrentBrewery())];
-
-        List<Brewery> breweries = nearestRoute.getLocations().stream()
-                .map(GeoLocation::getBrewery)
-                .collect(Collectors.toList());
-
-        System.out.println(breweries);
-
-        List<BeerType> collectedBeer = breweries.stream()
-                .map(Brewery::getBeerTypes)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-
-        System.out.println(collectedBeer);
-
-
-
-        final long duration = System.nanoTime() - startTime;
-        return new Result(routeCost, listOfbreweriesToDto(breweries), listOfBeerTypeToDto(collectedBeer), duration);
+        return routeCost;
     }
 
+    private void printResultToConsole(double routeCost, List<Brewery> breweries, List<BeerType> collectedBeer, long duration) {
+        System.out.println("Found " + breweries.size() + " beer factories");
+        breweries.forEach(brewery ->
+            System.out.println(brewery.getId() + " " + brewery.getName()));
+
+        System.out.println("Collected " + collectedBeer.size() + " beer types");
+        collectedBeer.forEach(beer ->
+                System.out.println(beer.getName()));
+
+        System.out.println("Travelled distance " + routeCost);
+
+        System.out.println("Program took " + duration + " ms");
+    }
+
+    private List<Brewery> getListOfBreweries(Route nearestRoute) {
+        return nearestRoute.getLocations().stream()
+                .map(GeoLocation::getBrewery)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
     private double distanceToHome(double distanceToHome1) {
         return distanceToHome1;
     }
 
-    private Result buildResult(double routeCost, List<Brewery>breweries, List<BeerType>collectedBeer, long duration){
-        listOfbreweriesToDto(breweries);
-        listOfBeerTypeToDto(collectedBeer);
-        return new Result(routeCost, listOfbreweriesToDto(breweries), listOfBeerTypeToDto(collectedBeer), duration);
-    }
-
     private List<BreweryDto> listOfbreweriesToDto(List<Brewery> breweries){
         return breweries.stream()
-                .map(br -> BreweryToDto(br))
+                .map(br -> br.toDto())
                 .collect(Collectors.toList());
-    }
-
-    private BreweryDto BreweryToDto(Brewery brewery1) {
-        BreweryDto breweryDto = new BreweryDto();
-        breweryDto.setId(brewery1.getId());
-        breweryDto.setName(brewery1.getName());
-        breweryDto.setBeerTypes(brewery1.getBeerTypes());
-        breweryDto.setLocations(brewery1.getLocation());
-        return breweryDto;
     }
 
     private List<BeerTypeDto>listOfBeerTypeToDto(List<BeerType>beerTypes){
         return beerTypes.stream()
-                .map(beer -> BeerTypeToDto(beer))
+                .map(beer -> beer.toDto())
                 .collect(Collectors.toList());
     }
 
-    private BeerTypeDto BeerTypeToDto(BeerType beerType) {
-        BeerTypeDto beerTypeDto = new BeerTypeDto();
-        beerTypeDto.setId(beerType.getId());
-        beerTypeDto.setName(beerType.getName());
-        return beerTypeDto;
+    private List<BeerType> getListOfBeerTypes(List<Brewery> breweries) {
+        return breweries.stream()
+                .map(Brewery::getBeerTypes)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
+
+
 }
 
 
