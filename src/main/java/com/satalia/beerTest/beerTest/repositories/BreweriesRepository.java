acb94ce9ab7d935baf529fa23464575bf46ee47e
TomasV10 <tomasvarnauskas19@gmail.com>
2020-03-08 15:33:31 +0200

    initial commit

--- src/main/java/com/satalia/beerTest/beerTest/repositories/BreweriesRepository.java
+++ src/main/java/com/satalia/beerTest/beerTest/repositories/BreweriesRepository.java
@@ -1,6 +1,6 @@
 package com.satalia.beerTest.beerTest.repositories;
 
-import com.satalia.beerTest.beerTest.entities.Brewery;
+import com.satalia.beerTest.beerTest.brewery.Brewery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BreweriesRepository extends JpaRepository<Brewery, Long> {
--- src/main/java/com/satalia/beerTest/beerTest/repositories/LocationsRepository.java
+++ src/main/java/com/satalia/beerTest/beerTest/repositories/LocationsRepository.java
@@ -1,6 +1,6 @@
 package com.satalia.beerTest.beerTest.repositories;
 
-import com.satalia.beerTest.beerTest.entities.GeoLocation;
+import com.satalia.beerTest.beerTest.brewery.GeoLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationsRepository extends JpaRepository<GeoLocation, Long> {
