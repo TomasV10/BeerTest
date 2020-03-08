dc4b9650ce239c81ff93ae58374ed1f787d92c6d
TomasV10 <tomasvarnauskas19@gmail.com>
2020-03-08 15:37:19 +0200

    arranged file

--- src/test/java/com/satalia/beerTest/beerTest/DistanceMatrixCreatorTest.java
+++ src/test/java/com/satalia/beerTest/beerTest/DistanceMatrixCreatorTest.java
@@ -1,7 +1,7 @@
 package com.satalia.beerTest.beerTest;
 
-import com.satalia.beerTest.beerTest.entities.GeoLocation;
-import com.satalia.beerTest.beerTest.mainTask.DistanceMatrixCreator;
+import com.satalia.beerTest.beerTest.brewery.GeoLocation;
+import com.satalia.beerTest.beerTest.distanceMatrix.DistanceMatrixCreator;
 import com.satalia.beerTest.beerTest.services.DistanceService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -9,14 +9,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.mockito.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
 public class DistanceMatrixCreatorTest {
+
     @Mock
     private DistanceService distanceService;
 
@@ -31,8 +31,6 @@ public class DistanceMatrixCreatorTest {
                 .thenReturn(2D);
 
         double[][] matrix = distanceMatrixCreator.calculateDistanceUsingMatrix(distances);
-
-        System.out.println(Arrays.deepToString(matrix));
 
         Assert.assertEquals(0D, matrix[0][0], 0d);
         Assert.assertEquals(1D, matrix[0][1], 0d);
--- src/test/java/com/satalia/beerTest/beerTest/DistanceServiceTest.java
+++ src/test/java/com/satalia/beerTest/beerTest/DistanceServiceTest.java
@@ -1,6 +1,6 @@
 package com.satalia.beerTest.beerTest;
 
-import com.satalia.beerTest.beerTest.entities.GeoLocation;
+import com.satalia.beerTest.beerTest.brewery.GeoLocation;
 import com.satalia.beerTest.beerTest.services.DistanceService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -33,13 +33,19 @@ public class DistanceServiceTest {
 
     @Test
     public void calculateDistanceTest(){
-        double actualDistance =  distanceService.calculateDistance(51.74250300, 19.43295600,51.74250300, 19.43295600);
+        double actualDistance =  distanceService.calculateDistance(51.74250300, 19.43295600,
+                51.74250300, 19.43295600);
+
         Assert.assertEquals(0, actualDistance, 0d);
 
-        double actualDistance1 = distanceService.calculateDistance(51.74250300, 19.43295600, 49.96220016, 20.60029984);
+        double actualDistance1 = distanceService.calculateDistance(51.74250300, 19.43295600,
+                49.96220016, 20.60029984);
+
         Assert.assertEquals(214, actualDistance1, 0d);
 
-        double actualDistance2 = distanceService.calculateDistance(49.96220016, 20.60029984, 52.673900060,14.90040016);
+        double actualDistance2 = distanceService.calculateDistance(49.96220016, 20.60029984,
+                52.673900060,14.90040016);
+
         Assert.assertEquals(498, actualDistance2, 0d);
     }
 }
