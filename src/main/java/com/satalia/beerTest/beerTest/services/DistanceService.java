2bec4c810e9e26ef366d0c91496492b3f42a2ff2
TomasV10 <tomasvarnauskas19@gmail.com>
2020-03-08 15:34:46 +0200

    initial commit

--- src/main/java/com/satalia/beerTest/beerTest/services/DistanceService.java
+++ src/main/java/com/satalia/beerTest/beerTest/services/DistanceService.java
@@ -1,11 +1,10 @@
 package com.satalia.beerTest.beerTest.services;
 
-import com.satalia.beerTest.beerTest.entities.GeoLocation;
+import com.satalia.beerTest.beerTest.brewery.GeoLocation;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 @Service
